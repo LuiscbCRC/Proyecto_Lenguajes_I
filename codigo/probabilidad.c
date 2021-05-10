9dd1f8a1d95049ac8132b5a5a73c1ba5fe40f17b
valeryvl08 <54646505+valeryvl08@users.noreply.github.com>
2021-05-09 19:57:14 -0600

    Probabilidades
    contiene la funcion media empirica y variancia poblacional

---
+++ codigo/probabilidad.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <math.h>
+
+
+double mediaEmpirica(double probabilidades[], int size){
+    double suma=0;
+
+    for (int i = 0; i < size; i++)
+    {
+        suma+=probabilidades[i];
+    }
+    
+    return suma/size;
+    
+}
+
+//Variancia POBLACIONAL
+double varianciaEmpirica(double probabilidades[], int size, double media){
+    double suma=0;
+
+    for (int i = 0; i < size; i++)
+    {
+        suma+= pow(probabilidades[i]-media,2.0);
+    }
+    
+    return suma/size;
+}
+
